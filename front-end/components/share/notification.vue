c59998ceb8013f8f682455e163558bd56f9a4da3
Thaycacac <thaycacac@gmail.com>
2018-10-30 22:47:22 +0700

    [add] notification

---
+++ front-end/components/share/notification.vue
@@ -0,0 +1,205 @@
+<template>
+  <div class="modal mark-modal" id="markModal">
+    <div class="cart-content modal-content-1" id="markModalContent">
+      <h6 class="cart-title">Giỏ hàng của bạn (69 sản phẩm)<i class="fas fa-times markIconClose" id="iconCloseModal"></i></h6>
+      <div class="wrapper-cart">
+        <div class="one-cart">
+          <div class="one-cart-title my-2">Giỏ hàng 1</div>
+          <div class="list-cart">
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="total-one-cart pb-3 pt-2">
+            <div class="total">Tổng: 120.000VND</div><button class="payment">Thanh toán</button>
+          </div>
+        </div>
+      </div>
+      <div class="wrapper-cart">
+        <div class="one-cart">
+          <div class="one-cart-title my-2">Giỏ hàng 1</div>
+          <div class="list-cart">
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="total-one-cart pb-3 pt-2">
+            <div class="total">Tổng: 120.000VND</div><button class="payment">Thanh toán</button>
+          </div>
+        </div>
+      </div>
+      <div class="wrapper-cart">
+        <div class="one-cart">
+          <div class="one-cart-title my-2">Giỏ hàng 1</div>
+          <div class="list-cart">
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="cart-container py-3">
+              <div class="media"><img class="mr-3 cart-image img-fluid" alt="cart image" src="~/assets/images/user.jpg" />
+                <div class="media-body">
+                  <h5 class="mt-0 mb-1 product-name">Thịt nướng</h5>
+                  <h5 class="my-0 product-price"><span>150.000đ</span></h5>
+                  <div class="product-action mt-2"><i class="fas fa-minus btn-action btn-icon-cart-dec"></i><span class="product-number">1</span><i
+                      class="fas fa-plus btn-action btn-icon-cart-inc"></i>
+                    <div class="total-price ml-3">150.000d</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="total-one-cart pb-3 pt-2">
+            <div class="total">Tổng: 120.000VND</div><button class="payment">Thanh toán</button>
+          </div>
+        </div>
+      </div>
+      <div class="mt-4"></div>
+      <div class="cart-provider">
+        <div class="title-provider">Nhà cung cấp 1</div>
+        <ul class="provider-content">
+          <li class="content-item">Miễn phí giao hàng nội thành cho đơn hàng trên 300,000đ và ngoại thành cho đơn hàng
+            trên 500,000đ</li>
+          <li class="content-item">Giảm 5% cho các đơn hàng giao chiều đối với rau củ và trái cây</li>
+        </ul>
+      </div>
+      <div class="cart-provider">
+        <div class="title-provider">Nhà cung cấp 1</div>
+        <ul class="provider-content">
+          <li class="content-item">Miễn phí giao hàng nội thành cho đơn hàng trên 300,000đ và ngoại thành cho đơn hàng
+            trên 500,000đ</li>
+          <li class="content-item">Giảm 5% cho các đơn hàng giao chiều đối với rau củ và trái cây</li>
+        </ul>
+      </div>
+      <div class="reset-cart text-center"><button class="btn-reset-cart btn"><i class="fas fa-sync-alt mr-2"></i> Cập
+          nhật giỏ hàng</button></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '~assets/scss/variable.scss';
+@import '~assets/scss/share/notification.scss';
+</style>
---
+++ front-end/pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <notification />
+  </div>
+</template>
+
+<script>
+import Notification from '../components/share/notification';
+export default {
+  components: {
+    Notification
+  }
+}
+</script>
