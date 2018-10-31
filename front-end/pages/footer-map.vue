57e3d2535ba6e3b967e16d21d3efb941e0bffdab
thaycacac <thaycacac@gmail.com>
2018-10-31 23:16:10 +0700

    [ADD]footer map page

---
+++ front-end/pages/footer-map.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer-map></footer-map>
+</template>
+
+<script>
+import FooterMap from '../components/share/footer-map/footer';
+export default {
+  components:{
+      FooterMap
+  }
+}
+</script>
+
+<style>
+
+</style>
--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -50,7 +50,8 @@
         pages: [
           { no: '1', name: 'Thông tin tài khoản', link: '/profile' },
           { no: '2', name: 'Đổi mật khẩu', link: '/change-password' },
-          { no: '3', name: 'Lịch sử giao dịch',link: '/history'}
+          { no: '3', name: 'Lịch sử giao dịch',link: '/history'},
+          { no: '4', name: 'Footer map', link: '/footer-map'}
         ]
       }
     },
