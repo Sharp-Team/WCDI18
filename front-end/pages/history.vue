ef883c763d6ee59604e453aa543f7950e2f9df11
thaycacac <thaycacac@gmail.com>
2018-11-01 00:42:18 +0700

    [Upload]history

---
+++ front-end/pages/history.vue
@@ -0,0 +1,12 @@
+<template>
+    <history></history>
+</template>
+
+<script>
+import History from './../components/share/history/history'
+export default {
+    components:{
+        History
+    }
+}
+</script>
