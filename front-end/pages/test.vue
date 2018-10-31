746c8d3a1ce7d6a32500718ef11a03af623ac980
Thaycacac <thaycacac@gmail.com>
2018-10-31 14:33:05 +0700

    [add] button custom

--- front-end/pages/test.vue
+++ front-end/pages/test.vue
@@ -1,14 +1,111 @@
 <template>
   <div>
-    <notification />
+  <div class="button">
+    <span class="button__mask"></span>
+    <span class="button__text">Đăng Nhập</span>
+    <span class="button__text button__text--bis">Đăng Nhập</span>
   </div>
+
+  <br>
+
+  <div class="button">
+    <span class="button__mask"></span>
+    <span class="button__text">button with long text</span>
+    <span class="button__text button__text--bis">button with long text</span>
+  </div>
+
+</div>
 </template>
 
 <script>
-import Notification from '../components/share/notification';
-export default {
-  components: {
-    Notification
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/variable.scss';
+$anim-text-offset: 1em;
+.button{
+  user-select: none;
+  display: inline-block;
+  border: 0;
+  position: relative;
+  cursor: pointer;
+  overflow: hidden;
+  opacity: 1;
+  color: #FFF;
+  background-color: $color-main;
+  border-radius: 4px;
+  &__text{
+    display: block;
+        padding: 8px 25px;
+    font-weight: 500;
+    &:before{
+      content: attr(title);
+    }
+    &--bis{
+      display: block;
+      position: absolute;
+      top: 0; left:0; right: 0; bottom: 0;
+      transform: translateX(-1 * $anim-text-offset);
+      opacity: 0;
+    }
+  }
+  &__mask{
+    display: block;
+    position: absolute;
+    top: 0; left: 0; right: 0; bottom: 0;
+    background: white;
+    transform: translateX(-100%) rotate(45deg);
+    transition: all 0.3s;
   }
 }
-</script>
+
+.button:hover{
+  opacity: 1;
+  .button__text{
+    animation: fx-text .3s ease-out;
+    &--bis{
+      animation: fx-text-bis .3s ease-out;
+    }
+  }
+  .button__mask{
+    animation: fx-mask .3s ease-out;
+  }
+}
+
+.button:active{
+  opacity: 1;
+  background: white;
+  color: inherit;
+}
+
+@keyframes fx-mask {
+  0%{
+    transform: translateX(-100%) rotate(45deg);
+  }
+  100%{
+    transform: translateX(100%) rotate(45deg);
+  }
+}
+
+@keyframes fx-text {
+  0%{
+    transform: translateX(0);
+    opacity: 1;
+  }
+  100%{
+    transform: translateX($anim-text-offset);
+    opacity: 0;
+  }
+}
+@keyframes fx-text-bis {
+  0%{
+    transform: translateX(-1 * $anim-text-offset);
+    opacity: 0;
+  }
+  100%{
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+</style>
+
