11673b565025e190f68134540c4d672593e7e735
Quang Hiep <bacodekiller@gmail.com>
2018-11-01 18:22:24 +0700

    [Update] nuxt-link for home

--- front-end/components/header/navbar.vue
+++ front-end/components/header/navbar.vue
@@ -7,9 +7,7 @@
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="/">
-              Trang chủ
-            </a>
+            <nuxt-link to="/" class="nav-link">Trang chủ</nuxt-link>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">
