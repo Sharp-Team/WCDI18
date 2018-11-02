4a557f114b2ff0a270df134880e968bcc99af1cf
Thaycacac <thaycacac@gmail.com>
2018-11-02 16:40:07 +0700

    [test]  buefy

--- front-end/nuxt.config.js
+++ front-end/nuxt.config.js
@@ -76,7 +76,8 @@ module.exports = {
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
     // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt'
+    'bootstrap-vue/nuxt',
+    'nuxt-buefy'
   ],
   /*
   ** Axios module configuration
