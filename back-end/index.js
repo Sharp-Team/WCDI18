0ba83250998e0aefd12a8d716aa2530bb40ff7ce
Thaycacac <thaycacac@gmail.com>
2018-11-06 12:00:33 +0700

    [demo] test back

--- back-end/index.js
+++ back-end/index.js
@@ -3,28 +3,32 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const User = require('./server/src/components/user/userRouter')
-
 require('dotenv').config()
 
 const app = express()
 
 app.use(morgan('combined'))
-app.use(bodyParser.urlencoded({
-	extended: true
-}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(cors())
 
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.CONNECT_MONGO, {
-	useNewUrlParser: true
+  useNewUrlParser: true
 }).then(() => {
 	console.log('Connect Db')
-}).catch((err) => console.log(err))
+}).catch((err) =>  console.log(err))
 
 app.use('/user', User)
 
+const user = {
+  name: 'Hoa Mat Lol',
+  age: -20
+}
+
+app.get('/', (req, res) => {
+  res.status(200).json(user)
+})
 const port = process.env.PORT
 app.listen(port, () => {
-	console.log(`Server is running on port ${port} !`)
+  console.log(`Server is running on port ${port} !`)
 })
