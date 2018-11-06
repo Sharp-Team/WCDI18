9feae5fac0ddfca7cddccb073a9f2b16f9c02a56
thaycacac <thaycacac@gmail.com>
2018-11-06 10:55:32 +0700

    [Update] api

--- back-end/index.js
+++ back-end/index.js
@@ -1,26 +1,30 @@
-const bodyParser = require('body-parser')
-const express = require('express')
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const User = require('./testServer/src/components/user/userRouter')
+const bodyParser = require('body-parser'),
+	express = require('express'),
+	morgan = require('morgan'),
+	cors = require('cors'),
+	mongoose = require('mongoose'),
+	User = require('./testServer/src/components/user/userRouter')
 require('dotenv').config()
 
 const app = express()
 
 app.use(morgan('combined'))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
 app.use(bodyParser.json())
+app.use(cors())
 
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.CONNECT_MONGO, {
-  useNewUrlParser: true
+	useNewUrlParser: true
 }).then(() => {
 	console.log('Connect Db')
-}).catch((err) =>  console.log(err))
+}).catch((err) => console.log(err))
 
-app.use('/api/user', User)
+app.use('/user', User)
 
 const port = process.env.PORT
 app.listen(port, () => {
-  console.log(`Server is running on port ${port} !`)
+	console.log(`Server is running on port ${port} !`)
 })
