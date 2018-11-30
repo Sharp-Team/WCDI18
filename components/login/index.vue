d30de768ad88cd315401fe9d191f65c875c93487
Thaycacac <thaycacac@gmail.com>
2018-12-01 00:30:33 +0700

    [update]fix login facebook

--- components/login/index.vue
+++ components/login/index.vue
@@ -130,24 +130,26 @@ export default {
     }
   },
   beforeMount() {
-    window.fbAsyncInit = (function() {
-      // FB.init({
-      //   appId: '2313158672248133',
-      //   cookie: true,
-      //   xfbml: true,
-      //   version: 'v2.8'
-      // })
-    })(
-      (function(d, s, id) {
-        var js,
-          fjs = d.getElementsByTagName(s)[0]
-        if (d.getElementById(id)) return
-        js = d.createElement(s)
-        js.id = id
-        js.src = '//connect.facebook.net/en_US/sdk.js'
-        fjs.parentNode.insertBefore(js, fjs)
-      })(document, 'script', 'facebook-jssdk')
-    )
+    // eslint-disable-next-line
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: '2313158672248133',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.8'
+      })
+      // eslint-disable-next-line
+    };
+    // eslint-disable-next-line
+    (function(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) return
+      js = d.createElement(s)
+      js.id = id
+      js.src = '//connect.facebook.net/en_US/sdk.js'
+      fjs.parentNode.insertBefore(js, fjs)
+    })(document, 'script', 'facebook-jssdk')
   },
   methods: {
     USER_SIGNIN,
